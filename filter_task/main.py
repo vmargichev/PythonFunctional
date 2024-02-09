6ba627839d022889c170a1273dc774681ae30836
vmargichev <93369733+vmargichev@users.noreply.github.com>
2024-02-09 23:34:45 +0200

    Trying the oposite

--- filter_task/main.py
+++ filter_task/main.py
@@ -1,6 +1,2 @@
 def remove_invalid_lines(document):
-    # lines = document.splitlines(True)
-    # for line in lines:
-    #     if line.startswith("-"):
-    #         lines.remove(line)
-    return "\n".join(filter(lambda x: not x.startswith("-"), document.split("\n")))
+    return "\n".join(filter(lambda x: not x.startswith('*'), document.split('\n')))
