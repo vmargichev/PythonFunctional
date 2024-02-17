074db6c0a7d5124a03af510bc370f3f2fa9e702a
vmargichev <93369733+vmargichev@users.noreply.github.com>
2024-02-17 16:08:49 +0200

    Completed Task

--- no-op_task/main.py
+++ no-op_task/main.py
@@ -2,12 +2,21 @@ def markdown_to_text(doc_content):
     last = list(map(lambda x: x.lstrip('# '), doc_content.split('\n')))
     last2 = remove_asterix_from_words(last)
     return last2
-
 def remove_asterix_from_words(doc_content_list):
+    list2 = []
+    list3 = []
+    list4 = []
+    string = ''
     for line in doc_content_list:
-        words_in_line = line.split(' ')
-        for word in words_in_line:
-            if len(word) > 1 and '*' in word:
-                word.strip('*')
-                print(word)
-    return '\n'.join(doc_content_list)
+        list2.append(line.split(' '))
+    for sublist in list2:
+        sublist2 = []
+        for value in sublist:
+            if len(value) > 1:
+                sublist2.append(value.strip('*'))
+            else:
+               sublist2.append(value) 
+        list3.append(sublist2)
+    for line in list3:
+        list4.append(' '.join(line))
+    return '\n'.join(list4)
